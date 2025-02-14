42fd786ce8d11a56629c35506f89166c9611dcc2
Bryan Nguyen <bryan.nguyen250@gmail.com>
2025-02-14 15:59:42 -0800

    re-enable routing

--- src/main/kotlin/app/flock/social/plugins/Routing.kt
+++ src/main/kotlin/app/flock/social/plugins/Routing.kt
@@ -7,10 +7,17 @@ import app.flock.social.data.dao.FollowsDao
 import app.flock.social.data.dao.MailingListDao
 import app.flock.social.data.dao.RsvpDao
 import app.flock.social.data.dao.UsersDao
+import app.flock.social.route.bookmarksRoute
+import app.flock.social.route.communityRoute
+import app.flock.social.route.eventsRoute
+import app.flock.social.route.followsRoute
 import app.flock.social.route.mailingListRoutes
+import app.flock.social.route.paymentRoutes
+import app.flock.social.route.rsvpRoute
+import app.flock.social.route.supabaseAuthRoutes
+import app.flock.social.route.usersRoute
 import io.ktor.server.application.Application
 import io.ktor.server.routing.routing
-import org.slf4j.LoggerFactory
 
 fun Application.configureRouting() {
     val eventDao = EventDao()
@@ -19,18 +26,18 @@ fun Application.configureRouting() {
     val usersDao = UsersDao()
     val bookmarksDao = BookmarkDao()
     val followsDao = FollowsDao()
-    val logger = LoggerFactory.getLogger(this::class.java)
+//    val logger = LoggerFactory.getLogger(this::class.java)
     val mailingListDao = MailingListDao()
 
     routing {
-//        paymentRoutes()
+        paymentRoutes()
         mailingListRoutes(mailingListDao)
-//        supabaseAuthRoutes()
-//        eventsRoute(eventDao = eventDao,)
-//        rsvpRoute(rsvpDao = rsvpDao,)
-//        communityRoute(communityDao = communityDao,)
-//        bookmarksRoute(bookmarksDao = bookmarksDao,)
-//        followsRoute(followsDao = followsDao)
-//        usersRoute(usersDao = usersDao)
+        supabaseAuthRoutes()
+        eventsRoute(eventDao = eventDao,)
+        rsvpRoute(rsvpDao = rsvpDao,)
+        communityRoute(communityDao = communityDao,)
+        bookmarksRoute(bookmarksDao = bookmarksDao,)
+        followsRoute(followsDao = followsDao)
+        usersRoute(usersDao = usersDao)
     }
 }
